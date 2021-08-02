d0997c16cc84d80610915277b1e7afe77ab1a0d5
mateusnobre <mateusnobrests@gmail.com>
2021-08-02 03:56:15 -0300

    test files and edit database deletion after tests

--- tests/utils/database.ts
+++ tests/utils/database.ts
@@ -1,7 +1,13 @@
 import { getRepository } from "typeorm";
 
-import User from "../../src/entities/User";
+import Category from "../../src/entities/Category";
+import Course from "../../src/entities/Course";
+import Exam from "../../src/entities/Exam";
+import Lecturer from "../../src/entities/Lecturer";
 
 export async function clearDatabase () {
-  await getRepository(User).delete({});
+  await getRepository(Category).delete({});
+  await getRepository(Course).delete({});
+  await getRepository(Exam).delete({});
+  await getRepository(Lecturer).delete({});
 }
